71187215e32d528501fddb54a716e9af8eb40a27
Javier Méndez Gómez <novdomo@gmail.com>
2020-01-31 22:26:28 +0100

    Clase Vehiculo

--- js/classes/Vehiculo.js
+++ js/classes/Vehiculo.js
@@ -1,3 +1,22 @@
 class Vehiculo{
 
+    constructor(matricula,marca,modelo,tipo,añoFabricacion){
+	this.idVehiculo=modelo.contadorVehiculos+1;
+	modelo.contadorVehiculos++;
+	this.matricula;
+	this.marca;
+	this.modelo;
+	this.tipo;
+	this.anyoFabricacion;
+    }
+
+    toHTMLTableRow(){
+	let oFila=document.createElement("TABLE");
+	oFila.insertCell(-1).textContent=idVehiculo;
+	oFila.insertCell(-1).textContent=matricula;
+	oFila.insertCell(-1).textContent=marca;
+	oFila.insertCell(-1).textContent=modelo;
+	oFila.insertCell(-1).textContent=tipo;
+	oFila.insertCell(-1).textContent=anyoFabricacion;
+    }
 }
